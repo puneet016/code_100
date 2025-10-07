0a8703d1ab90a87c140723e4ef2b83b41c869699
puneet016 <puneetmulgund16@gmail.com>
2025-10-08 00:24:38 +0530

    initial commit

---
+++ groupnamegen.py
@@ -0,0 +1,4 @@
+print("Wellcome to the group name generator")
+colour=input("what is your favourite colour?\n")
+animals=input("what is your favourite animal?\n")
+print("Your group name could be "+ colour + " " + animals)
