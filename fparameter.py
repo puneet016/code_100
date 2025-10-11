1be87113646caae48a035853dcd8a560430539f7
puneet016 <puneetmulgund16@gmail.com>
2025-10-11 22:47:21 +0530

    initial commit

---
+++ fparameter.py
@@ -0,0 +1,10 @@
+def great():
+    print("Hello, world!")
+    print("How are you today?")
+great()
+
+
+def greet_user(name):
+    print(f"Hello, {name}!")
+    print("How are you today?")
+greet_user("Alice")
