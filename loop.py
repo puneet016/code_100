f0f1c9a3b549a95a0f361f77bdd9c22ee5df137c
puneet016 <puneetmulgund16@gmail.com>
2025-10-16 23:13:40 +0530

    initial commit

---
+++ loop.py
@@ -0,0 +1,15 @@
+
+#Range function
+for number in range(1,10):
+    print(number)
+
+
+
+
+
+vegetables={"corrot","potato","tomato","onion"}
+for vegetable in vegetables:
+    print(vegetable)
+
+
+            
