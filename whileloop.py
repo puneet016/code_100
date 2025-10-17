f46c0da82adcc7546468df66eeea66c2a5b24160
puneet016 <puneetmulgund16@gmail.com>
2025-10-17 18:16:26 +0530

    initial commit

---
+++ whileloop.py
@@ -0,0 +1,21 @@
+def turn_right():
+     turn_left() 
+     turn_left() 
+     turn_left()
+
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+number_of _hurdles=6
+while number_of hurdles>0:
+    jump()
+    number+of _huedles-=1
+    print(nummber_of_hurdles)
