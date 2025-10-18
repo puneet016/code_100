104118aa1f4986ac040c99b7bf40916b44ad62ab
puneet016 <puneetmulgund16@gmail.com>
2025-10-18 22:09:07 +0530

    Create continue&break.py

---
+++ continue&break.py
@@ -0,0 +1,12 @@
+for item in list_of_items:
+    #do something to each item
+    if condition:
+        continue
+    #then do that inside loop
+    
+
+    while somethiing_that_is_true:
+    #do something repeatedly
+     if condition:
+         continue
+    #then do that inside while loop
