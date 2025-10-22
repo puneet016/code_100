06fa57c30b521212ed907431b300537d76495bc1
puneet016 <puneetmulgund16@gmail.com>
2025-10-22 22:29:21 +0530

    commit initial

---
+++ string.py
@@ -0,0 +1,17 @@
+name="punit"
+letter=name[0]
+print(letter)  # Output: 'p'
+length=len(name)
+print(length)  # Output: 5
+fruit="banana"
+l=len(fruit)
+print=(-l)  # Output: -66]
+
+print(fruit[:3])  # Output: 'ban'
+print(fruit[3:])  # Output: 'ana'
+print(fruit[:])  # Output: 'banana'
+greet="Hello World"
+print(greet.lower())  # Output: 'hello world'
+print(greet.upper())  # Output: 'HELLO WORLD'
+print(greet.islower())  # Output: False
+print(greet.isupper())  # Output: False 
