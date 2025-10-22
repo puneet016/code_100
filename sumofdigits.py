3028b0698dbd1234f11e0252afebbd0b73ac8436
puneet016 <puneetmulgund16@gmail.com>
2025-10-22 22:41:18 +0530

    initial commit

---
+++ sumofdigits.py
@@ -0,0 +1,6 @@
+def sum_of_two_digit():
+
+    two_digit_number=int(input("Enter a two-digit number: "))
+    sum_of_digits=int(two_digit_number[0])+int(two_digit_number[1])
+    return sum_of_digits      
+print(sum_of_two_digit())
