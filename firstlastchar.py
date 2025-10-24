d8c00d5f491b2be6931aaa390c077c616e9cb575
puneet016 <puneetmulgund16@gmail.com>
2025-10-24 22:10:18 +0530

    initial commit

---
+++ firstlastchar.py
@@ -0,0 +1,7 @@
+def first_last_characters(word):
+    if len(word)<2:
+        return ''
+    first_two_chars=word[0:2]
+    last_two_chars=word[-2:]
+    print(first_two_chars+last_two_chars)
+first_last_characters("appmillers")
