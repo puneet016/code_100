c3a88f8cd97d373a8994c43d1b9961d3c440e483
puneet016 <puneetmulgund16@gmail.com>
2025-10-24 21:45:37 +0530

    initial commit

---
+++ stringop.py
@@ -0,0 +1,14 @@
+str1="hello" 
+str2="world"
+print(str1 + " " + str2)  # Concatenation of two strings with a space in between
+
+print ('w' in str2)
+new_string=input("Enter a string: ")
+if new_string.startswith("Hello"):
+    print("The string starts with 'Hello'")
+else:
+    print("The string does not start with 'Hello'")
+
+new_string2="hello world"
+new_string2=new_string2[1:]
+print(new_string2)
