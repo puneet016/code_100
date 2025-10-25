28cedfa3ad595413337da375dc2edec2fd0ad0df
puneet016 <puneetmulgund16@gmail.com>
2025-10-25 23:10:50 +0530

    initial commit

---
+++ stringmethod.py
@@ -0,0 +1,14 @@
+my_name='EEADADA Doe'
+my_name1=my_name.lower()
+print(my_name1)
+my_name1=my_name.upper()
+print(my_name1)
+index=my_name.index('D')
+print(index)
+index=my_name.index('e')
+print(index)
+index=my_name.find('A',5)
+print(index)
+
+
+
