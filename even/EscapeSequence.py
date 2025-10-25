42fb929e831514eb00e62bb75eb0cebb7aae809c
puneet016 <puneetmulgund16@gmail.com>
2025-10-25 23:57:12 +0530

    initial commit

---
+++ even/EscapeSequence.py
@@ -0,0 +1,9 @@
+#triple quotes
+question='''He said "what's your name?"'''
+print(question)
+#escapindg the single quote
+question='He said "what\'s your name?"' 
+print(question)
+#escaping the double quotes
+question="He said \"what's your name?\""    
+print(question)
