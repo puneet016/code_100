d4a76cc9dd8b89edfbf74df2ada4b96e446eae40
puneet016 <puneetmulgund16@gmail.com>
2025-10-28 18:46:05 +0530

    list

---
+++ even/list.py
@@ -0,0 +1,9 @@
+number=[10,20,30,40,50,60,70,80,90,100]
+names=["Alice","Bob","Charlie","David","Eva","Frank","Grace","Hannah","Ian","Jack"]
+empty_list=[]
+custom_string="hello"
+print(names[0])
+print(names[2+1])
+for index in range (len(number)):
+    number[index]=number[index]+5
+print(number)
---
+++ even/p.py
@@ -0,0 +1,5 @@
+size=5
+for i in range(size):
+    count=size-i
+    print("*"*(i+1))
+    print("*"*count)
