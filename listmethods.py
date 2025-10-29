2a8b299e90a9c4e2e08840133a6c2cef78fed6f2
puneet016 <puneetmulgund16@gmail.com>
2025-10-29 22:47:18 +0530

    list

---
+++ listmethods.py
@@ -0,0 +1,17 @@
+#index
+animal=['doh','cat','bat','rat']
+index=animal.index('bat')
+print(index)
+
+animal=['doh','cat','bat','rat','bat']
+index=animal.index('bat',3,5)
+print(index)
+
+
+#Append Method
+animal=['dog','cat','bat']
+animal.append('rat')
+print(animal)
+
+#Extend Method
+animal=['dog','cat','bat']
