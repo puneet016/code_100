d8421ed92acdf79570c467479a8c2a95ff736797
puneet016 <puneetmulgund16@gmail.com>
2025-10-30 19:43:30 +0530

    initial commit

--- listmethods.py
+++ listmethods.py
@@ -15,3 +15,55 @@ print(animal)
 
 #Extend Method
 animal=['dog','cat','bat']
+animal.extend(['rat','elephant'])
+print(animal)
+#Insert Method
+animal=['dog','cat','bat']
+animal.insert(1,'rat')
+print(animal)
+
+#Remove Method
+animal=['dog','cat','bat']
+animal.remove('cat')
+print(animal)
+
+#Pop Method
+animal=['dog','cat','bat']
+animal.pop(1)
+print(animal)
+
+#Clear Method
+animal=['dog','cat','bat']
+animal.clear()
+print(animal)
+
+#Sort Method
+animal=['dog','cat','bat']
+animal.sort()
+print(animal)
+
+#Reverse Method
+animal=['dog','cat','bat']
+animal.reverse()
+print(animal)
+
+#Copy Method
+animal=['dog','cat','bat']
+animal1=animal.copy()
+print(animal1)      
+#Count Method
+animal=['dog','cat','bat','dog']    
+count=animal.count('dog')
+print(count)
+#Join Method
+animal=['dog','cat','bat']
+result=' '.join(animal) 
+print(result)
+#Split Method
+text="dog cat bat"
+result=text.split()
+print(result)   
+#Length Method
+animal=['dog','cat','bat']
+length=len(animal)
+print(length)
---
+++ update.py
@@ -0,0 +1,4 @@
+list1=[10,10,5,15,50,20,50]
+index=list1.index(50)
+list1[index]=5
+print(list1)
